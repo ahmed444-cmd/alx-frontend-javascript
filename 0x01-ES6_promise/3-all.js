138ea7445dd69c6d907ed80c459275186745ef9a
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-10-03 09:53:58 -0700

    mandatory

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,9 @@
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  // Invoke the uploadPhoto function to upload the user's photo & then chain all
-  // Combine the promises and manage them with a single error handling statement.
-  return Promise
-    .all([uploadPhoto(), createUser()])
-    .then((userResp) => {
-      // If the user creation is successful, log the body, firstName, and lastName to the console
-      console.log(`${userResp[0].body} ${userResp[1].firstName} ${userResp[1].lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
-    .catch(() => {
-      // If an error occurs, log "Signup system offline" to the console.
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
+}
