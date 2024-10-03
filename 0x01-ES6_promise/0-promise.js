b6d87115c42a9eec90df5b8fe4a1930785ec1f54
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-10-03 09:37:59 -0700

    mandatory

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  return new Promise(() => {
+    // return new Promise((resolve, reject) => {}) is also valid.
+    // Retrieve something, like data, by making a request.
+    // If it's successful, invoke the resolve function and pass the data along.
+    // In case of an error, call the reject function and pass the error.
+  });
+}
