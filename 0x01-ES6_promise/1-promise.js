e2b43568caa525aceac9fc7465c5c3a0ee61a954
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-10-03 09:39:49 -0700

    mandatory

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -7,7 +7,7 @@ export default function getFullResponseFromAPI(success) {
       });
     } else {
       reject(
-        new Error('The fk API is not working currently'),
+        new Error('The fake API is not working currently'),
       );
     }
   });
